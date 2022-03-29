58349ff7406bb8e2697780adcfa1ab4daa1d0567
Christopher Hernandez <cerberean661@gmail.com>
2022-03-28 21:13:16 -0700

    declaring elements, and testing

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+var notify = document.querySelector('.notify')
+var albums = document.querySelectorAll('.album')
+var itemsTotal = document.querySelector('.items-total')
+var saveBtn = document.querySelector('.button')
 
-
-
+console.log(albums)
 
 
 
